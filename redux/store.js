4c752f95d872b9ac15fbd0991db9742c956b0e4f
Bhanu Teja P <bhanuteja.p@swiggy.in>
2019-08-28 20:52:20 +0530

    Cleanup store.js

--- redux/store.js
+++ redux/store.js
@@ -9,15 +9,3 @@ import reducer from './reducers';
 const middleware = applyMiddleware(promise, thunk, logger);
 
 export default () => createStore(reducer, middleware);
-
-// store.dispatch({type: 'CHANGE_NAME', payload: 'Will'});
-// store.dispatch({type: 'CHANGE_AGE', payload: 36});
-// store.dispatch({type: 'INC'});
-// store.dispatch({type: 'DEC'});
-
-/* THUNK
-store.dispatch((dispatch) => {
-  // dispatch some action
-  // some http request with axios
-})
- */
